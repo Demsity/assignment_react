d8812f62a3f59d05cfa6aad2c9c3e8f3c445366d
Demsity <simonbacklin@hotmail.com>
2022-10-20 10:24:28 +0200

    fixed responsivness of navbar

--- src/Components/Buttons/ButtonRound.jsx
+++ src/Components/Buttons/ButtonRound.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const ButtonRound = ({icon, badge, link}) => {
+const ButtonRound = ({icon, badge, link, hideOnMobile,}) => {
+
   return (
-    <NavLink className='__round-button' to={link}>
+    <NavLink className={`__round-button ${hideOnMobile ? "hiddenOnMobile" : ""}`} to={link}>
       <i className={icon}></i>
       <span className='badge badge-pill badge-danger __badge-pill'>{badge}</span>
     </NavLink>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,6 +9,7 @@ import ButtonRound from '../Buttons/ButtonRound'
 const Navbar = () => {
 
   const [navbar, setNavbar] = useState(false)
+  const [showMenu, setShowMenu] = useState(true)
 
 
   const changeBackground = () => {
@@ -19,27 +20,38 @@ const Navbar = () => {
     }
   };
 
+  const toggleMenu = () => {
+      setShowMenu(!showMenu) 
+  }
 
   useEffect(() => {
+    // Make Menu appear hidden on app start, mobile view
+    if (window.innerWidth >= 576) {
+      setShowMenu(true)
+    }else {
+      setShowMenu(false)
+    }
+
     window.addEventListener('scroll', changeBackground, true);
     return () => window.removeEventListener('scroll', changeBackground);
   }, []);
 
   return (
-    <div className={navbar ? '__navbar-container-scrolled' : '__navbar-container'} >
+    <div className='__navbar-container' style={navbar ? {'backgroundColor': 'white'} : {'backgroundColor': 'none'}} >
       <div className='__navbar-wrapper container'>
       <NavLink className='__navbar-logo' to='/'>Fixxo.</NavLink>
-        <nav className='__navbar-links'>
+        <nav className='__navbar-links' style={showMenu ? {'right': '0'} : {'right': '-200px'}}>
             <NavLink end className='__navbar-link' to='/'>Home</NavLink>
             <NavLink end className='__navbar-link' to='/categories'>Categories</NavLink>
             <NavLink end className='__navbar-link' to='/products/'>Products</NavLink>
-            <NavLink end className='__navbar-link' to='/contact/'>Contacts</NavLink>
+            <NavLink end className='__navbar-link' to='/contact'>Contacts</NavLink>
         </nav>
         <nav className='__user-interface ms-4'>
             <ButtonRound link='/search' icon='fa-regular fa-magnifying-glass'  />
-            <ButtonRound link='/compare' icon='fa-light fa-code-compare'  />
-            <ButtonRound link='/favorites' icon='fa-light fa-heart' badge='1' />
+            <ButtonRound hideOnMobile={true} link='/compare' icon='fa-light fa-code-compare'  />
+            <ButtonRound hideOnMobile={true} link='/favorites' icon='fa-light fa-heart' badge='1' />
             <ButtonRound link='/cart' icon='fa-light fa-bag-shopping' badge='3' />
+            <button className='__hide-menu __round-button' onClick={toggleMenu}><i className="fa-regular fa-bars"></i></button>
         </nav>
       </div>
     </div>
