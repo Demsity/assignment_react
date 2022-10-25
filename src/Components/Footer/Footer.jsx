a3e309d7a3d7ff3203ef27702722cb016b9b483f
Demsity <simonbacklin@hotmail.com>
2022-10-25 13:19:51 +0200

    added footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <section className='__footer'>
+      <div className='container __footer-container'>
+        <div className='__footer-links'>
+          <a target='_blank' href="https://www.facebook.com"><i class="fa-brands fa-facebook"></i></a>
+          <a target='_blank' href="https://www.instagram.com"><i class="fa-brands fa-instagram"></i></a>
+          <a target='_blank' href="https://www.twitter.com"><i class="fa-brands fa-twitter"></i></a>
+          <a target='_blank' href="https://www.google.com"><i class="fa-brands fa-google"></i></a>
+          <a target='_blank' href="https://www.linkedin.com"><i class="fa-brands fa-linkedin"></i></a>
+        </div>
+        <div className='__footer-text'>
+          <p>© 2022 Fixxo. All Rights Reserved</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
